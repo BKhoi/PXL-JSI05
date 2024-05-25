197230f078e92396b482ef1340a45b13faa17b66
BKhoi <156518353+BKhoi@users.noreply.github.com>
2024-05-25 17:38:25 +0700

    register/log in

---
+++ b3/firebase.js
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQPJz9BT7vkKdsqBusApp0W4gQQDj7pMs",
+  authDomain: "fir-9c77e.firebaseapp.com",
+  projectId: "fir-9c77e",
+  storageBucket: "fir-9c77e.appspot.com",
+  messagingSenderId: "379374211188",
+  appId: "1:379374211188:web:bf251e225fa38c3c9dc121",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+export {
+  app,
+  auth,
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+};
