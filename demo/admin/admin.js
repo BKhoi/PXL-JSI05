d94de21abf4853b1233043fbdc0204407024ea33
BKhoi <156518353+BKhoi@users.noreply.github.com>
2024-07-20 21:05:51 +0700

    add to cart

--- demo/admin/admin.js
+++ demo/admin/admin.js
@@ -8,7 +8,13 @@ import {
   doc,
   updateDoc,
   getDocs,
-} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} from "https://www.gstatic.com/firebasejs/10.12.3/firebase-storage.js";
 
 import { app } from "../main/firebase.js";
 
@@ -22,19 +28,63 @@ form.addEventListener("submit", async (e) => {
   let bookdesc = document.getElementById("book-desc").value;
   let bookprice = document.getElementById("book-price").value;
   let bookinstock = document.getElementById("book-instock").value;
-  let bookimg = document.getElementById("book-img").value;
+  const img = document.getElementById("img1").files[0];
 
-  try {
-    const docRef = await addDoc(collection(db, "Books"), {
-      Booktitle: booktitle,
-      Bookdescription: bookdesc,
-      Bookprice: bookprice,
-      Bookinstock: bookinstock,
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
+  const storage = getStorage();
+  const imgPath = "Books/" + new Date().valueOf();
+  const storageRef = ref(storage, imgPath);
+
+  uploadBytes(storageRef, img1).then(async (snapshot) => {
+    console.log("Uploaded a blob or file!");
+    const url = await getDownloadURL(storageRef);
+
+    try {
+      const docRef = await addDoc(collection(db, "Books"), {
+        Booktitle: booktitle,
+        Bookdescription: bookdesc,
+        Bookprice: bookprice,
+        Bookinstock: bookinstock,
+        Bookimg: url,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  });
+});
+const postQuery = query(collection(db, "Books"));
+const show1 = document.getElementById("show1");
+onSnapshot(postQuery, (snapshot) => {
+  show1.innerHTML = "";
+  snapshot.forEach((doc) => {
+    const post = doc.data();
+    const postId = doc.id;
+    show1.innerHTML += `
+   <div class="col-md-4 col-12">
+    <div class="card" style="width: 18rem">
+      <a href="../details/detail.html">
+        <img
+          src="${post.Bookimg}"
+          class="card-img-top product-img"
+          alt="..."
+        />
+      </a>
+      <div class="card-body">
+        <h4 class="card-title name">${post.Booktitle}</h4>
+        <div class="card-text">
+          <p class="desc"><i> ${post.Bookdescription} </i></p>
+
+          <p></p>
+          <p id="price">Price: $${post.Bookprice}</p>
+          <p id="instock">In Stock: ${post.Bookinstock}</p>
+        </div>
+        <button class="delete-btn" onclick="deleteData('${doc.id}')">Delete</button>
+        <button onclick="editData('${doc.id}')">Edit</button>
+      </div>
+    </div>
+  </div>
+    `;
+  });
 });
 
 window.deleteData = async function (id) {
@@ -62,41 +112,6 @@ window.editData = async function (id) {
   }
 };
 
-const postQuery = query(collection(db, "Books"));
-const show1 = document.getElementById("show1");
-onSnapshot(postQuery, (snapshot) => {
-  show1.innerHTML = "";
-  snapshot.forEach((doc) => {
-    const post = doc.data();
-    const postId = doc.id;
-    show1.innerHTML += `
-     <div class="col-md-4 col-12">
-      <div class="card" style="width: 18rem">
-        <a href="../details/detail.html">
-          <img
-            src="${post.Bookimg}"
-            class="card-img-top product-img"
-            alt="..."
-          />
-        </a>
-        <div class="card-body">
-          <h4 class="card-title name">${post.Booktitle}</h4>
-          <div class="card-text">
-            <p class="desc"><i> ${post.Bookdescription} </i></p>
-
-            <p></p>
-            <p id="price">Price: $${post.Bookprice}</p>
-            <p id="instock">In Stock: ${post.Bookinstock}</p>
-          </div>
-          <button class="delete-btn" onclick="deleteData('${doc.id}')">Delete</button>
-          <button onclick="editData('${doc.id}')">Edit</button>
-        </div>
-      </div>
-    </div>
-      `;
-  });
-});
-
 const form2 = document.getElementById("form2");
 form2.addEventListener("submit", async (e) => {
   e.preventDefault();
@@ -104,18 +119,63 @@ form2.addEventListener("submit", async (e) => {
   let mangadesc = document.getElementById("manga-desc").value;
   let mangaprice = document.getElementById("manga-price").value;
   let mangainstock = document.getElementById("manga-instock").value;
+  const img2 = document.getElementById("img2").files[0];
 
-  try {
-    const docRef = await addDoc(collection(db, "Manga"), {
-      Mangatitle: mangatitle,
-      Mangadescription: mangadesc,
-      Mangaprice: mangaprice,
-      Mangainstock: mangainstock,
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
+  const storage = getStorage();
+  const imgPath = "Manga/" + new Date().valueOf();
+  const storageRef = ref(storage, imgPath);
+
+  uploadBytes(storageRef, img2).then(async (snapshot) => {
+    console.log("Uploaded a blob or file!");
+    const url = await getDownloadURL(storageRef);
+
+    try {
+      const docRef = await addDoc(collection(db, "Manga"), {
+        Mangatitle: mangatitle,
+        Mangadescription: mangadesc,
+        Mangaprice: mangaprice,
+        Mangainstock: mangainstock,
+        Mangaimg: url,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  });
+});
+const postQuery2 = query(collection(db, "Manga"));
+const show2 = document.getElementById("show2");
+onSnapshot(postQuery2, (snapshot) => {
+  show2.innerHTML = "";
+  snapshot.forEach((doc) => {
+    const post = doc.data();
+    const postId = doc.id;
+    show2.innerHTML += `
+     <div class="col-md-4 col-12">
+      <div class="card" style="width: 18rem">
+        <a href="../details/detail.html">
+          <img
+            src="${post.Mangaimg}"
+            class="card-img-top product-img"
+            alt="..."
+          />
+        </a>
+        <div class="card-body">
+          <h4 class="card-title name">${post.Mangatitle}</h4>
+          <div class="card-text">
+            <p class="desc"><i> ${post.Mangadescription}</i></p>
+
+            <p></p>
+            <p id="price">Price: $${post.Mangaprice}</p>
+            <p id="instock">In Stock: ${post.Mangainstock}</p>
+          </div>
+          <button class="delete-btn" onclick="deleteData2('${doc.id}')">Delete</button>
+          <button onclick="editData2('${doc.id}')">Edit</button>
+        </div>
+      </div>
+    </div>
+      `;
+  });
 });
 
 window.deleteData2 = async function (id) {
@@ -141,41 +201,6 @@ window.editData2 = async function (id) {
   }
 };
 
-const postQuery2 = query(collection(db, "Manga"));
-const show2 = document.getElementById("show2");
-onSnapshot(postQuery2, (snapshot) => {
-  show2.innerHTML = "";
-  snapshot.forEach((doc) => {
-    const post = doc.data();
-    const postId = doc.id;
-    show2.innerHTML += `
-     <div class="col-md-4 col-12">
-      <div class="card" style="width: 18rem">
-        <a href="../details/detail.html">
-          <img
-            src=""
-            class="card-img-top product-img"
-            alt="..."
-          />
-        </a>
-        <div class="card-body">
-          <h4 class="card-title name">${post.Mangatitle}</h4>
-          <div class="card-text">
-            <p class="desc"><i> ${post.Mangadescription}</i></p>
-
-            <p></p>
-            <p id="price">Price: $${post.Mangaprice}</p>
-            <p id="instock">In Stock: ${post.Mangainstock}</p>
-          </div>
-          <button class="delete-btn" onclick="deleteData2('${doc.id}')">Delete</button>
-          <button onclick="editData2('${doc.id}')">Edit</button>
-        </div>
-      </div>
-    </div>
-      `;
-  });
-});
-
 const form3 = document.getElementById("form3");
 form3.addEventListener("submit", async (e) => {
   e.preventDefault();
@@ -184,18 +209,64 @@ form3.addEventListener("submit", async (e) => {
   let scifidesc = document.getElementById("scifi-desc").value;
   let scifiprice = document.getElementById("scifi-price").value;
   let scifiinstock = document.getElementById("scifi-instock").value;
+  const img3 = document.getElementById("img3").files[0];
 
-  try {
-    const docRef = await addDoc(collection(db, "Scifi"), {
-      Scifititle: scifititle,
-      Scifidescription: scifidesc,
-      Scifiprice: scifiprice,
-      Scifiinstock: scifiinstock,
-    });
-    console.log("Document written with ID: ", docRef.id);
-  } catch (e) {
-    console.error("Error adding document: ", e);
-  }
+  const storage = getStorage();
+  const imgPath = "Scifi/" + new Date().valueOf();
+  const storageRef = ref(storage, imgPath);
+
+  uploadBytes(storageRef, img3).then(async (snapshot) => {
+    console.log("Uploaded a blob or file!");
+    const url = await getDownloadURL(storageRef);
+
+    try {
+      const docRef = await addDoc(collection(db, "Scifi"), {
+        Scifititle: scifititle,
+        Scifidescription: scifidesc,
+        Scifiprice: scifiprice,
+        Scifiinstock: scifiinstock,
+        Scifiimg: url,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  });
+});
+
+const postQuery3 = query(collection(db, "Scifi"));
+const show3 = document.getElementById("show3");
+onSnapshot(postQuery3, (snapshot) => {
+  show3.innerHTML = "";
+  snapshot.forEach((doc) => {
+    const post = doc.data();
+    const postId = doc.id;
+    show3.innerHTML += `
+   <div class="col-md-4 col-12">
+    <div class="card" style="width: 18rem">
+      <a href="../details/detail.html">
+        <img
+          src="${post.Scifiimg}"
+          class="card-img-top product-img"
+          alt="..."
+        />
+      </a>
+      <div class="card-body">
+        <h4 class="card-title name">${post.Scifititle}</h4>
+        <div class="card-text">
+          <p class="desc"><i> ${post.Scifidescription}</i></p>
+
+          <p></p>
+          <p id="price">Price: $${post.Scifiprice}</p>
+          <p id="instock">In Stock: ${post.Scifiinstock}</p>
+        </div>
+        <button class="delete-btn" onclick="deleteData3('${doc.id}')">Delete</button>
+        <button onclick="editData3('${doc.id}')">Edit</button>
+      </div>
+    </div>
+  </div>
+    `;
+  });
 });
 
 window.deleteData3 = async function (id) {
@@ -220,38 +291,3 @@ window.editData3 = async function (id) {
     console.error(error);
   }
 };
-
-const postQuery3 = query(collection(db, "Scifi"));
-const show3 = document.getElementById("show3");
-onSnapshot(postQuery3, (snapshot) => {
-  show3.innerHTML = "";
-  snapshot.forEach((doc) => {
-    const post = doc.data();
-    const postId = doc.id;
-    show3.innerHTML += `
-     <div class="col-md-4 col-12">
-      <div class="card" style="width: 18rem">
-        <a href="../details/detail.html">
-          <img
-            src=""
-            class="card-img-top product-img"
-            alt="..."
-          />
-        </a>
-        <div class="card-body">
-          <h4 class="card-title name">${post.Scifititle}</h4>
-          <div class="card-text">
-            <p class="desc"><i> ${post.Scifidescription}</i></p>
-
-            <p></p>
-            <p id="price">Price: $${post.Scifiprice}</p>
-            <p id="instock">In Stock: ${post.Scifiinstock}</p>
-          </div>
-          <button class="delete-btn" onclick="deleteData3('${doc.id}')">Delete</button>
-          <button onclick="editData3('${doc.id}')">Edit</button>
-        </div>
-      </div>
-    </div>
-      `;
-  });
-});
--- demo/main/index.js
+++ demo/main/index.js
@@ -5,6 +5,8 @@ import {
   collection,
   onSnapshot,
   doc,
+  addDoc,
+  getDoc,
 } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
 import {
   getAuth,
@@ -14,6 +16,8 @@ import {
   signOut,
 } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-auth.js";
 const auth = getAuth();
+const urlParams = new URLSearchParams(window.location.search);
+const postId = urlParams.get("id");
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
@@ -227,3 +231,141 @@ logoutbtn.addEventListener("click", () => {
 // });
 
 const db = getFirestore(app);
+
+const postQuery = query(collection(db, "Books"));
+const show1 = document.getElementById("show1");
+onSnapshot(postQuery, (snapshot) => {
+  show1.innerHTML = "";
+  snapshot.forEach((doc) => {
+    const post = doc.data();
+    const postId = doc.id;
+
+    show1.innerHTML += `
+      <div class="col-md-4 col-12">
+      <div class="card" style="width: 18rem">
+        <a href="../details/detail.html">
+          <img
+            src="${post.Bookimg}"
+            class="card-img-top product-img"
+            alt="..."
+          />
+        </a>
+        <div class="card-body">
+          <h4 class="card-title name">${post.Booktitle}</h4>
+          <div class="card-text">
+            <p class="desc"><i> ${post.Bookdescription} </i></p>
+
+            <p></p>
+            <p id="price">Price: $${post.Bookprice}</p>
+            <p id="instock">In Stock: ${post.Bookinstock}</p>
+          </div>
+        <button type="submit" id="add1" onclick="addCart1('${postId}')">Add to cart</button>
+        </div>
+      </div>
+    </div>
+      `;
+  });
+});
+
+const postQuery2 = query(collection(db, "Manga"));
+const show2 = document.getElementById("show2");
+onSnapshot(postQuery2, (snapshot) => {
+  show2.innerHTML = "";
+  snapshot.forEach((doc) => {
+    const post = doc.data();
+    const postId = doc.id;
+    show2.innerHTML += `
+     <div class="col-md-4 col-12">
+      <div class="card" style="width: 18rem">
+        <a href="../details/detail.html">
+          <img
+            src="${post.Mangaimg}"
+            class="card-img-top product-img"
+            alt="..."
+          />
+        </a>
+        <div class="card-body">
+          <h4 class="card-title name">${post.Mangatitle}</h4>
+          <div class="card-text">
+            <p class="desc"><i> ${post.Mangadescription}</i></p>
+
+            <p></p>
+            <p id="price">Price: $${post.Mangaprice}</p>
+            <p id="instock">In Stock: ${post.Mangainstock}</p>
+          </div>
+     <button type="submit" id="add" onclick="addCart('${postId}')">Add to cart</button>
+        </div>
+      </div>
+    </div>
+      `;
+  });
+});
+
+const postQuery3 = query(collection(db, "Scifi"));
+const show3 = document.getElementById("show3");
+onSnapshot(postQuery3, (snapshot) => {
+  show3.innerHTML = "";
+  snapshot.forEach((doc) => {
+    const post = doc.data();
+    const postId = doc.id;
+    show3.innerHTML += `
+   <div class="col-md-4 col-12">
+    <div class="card" style="width: 18rem">
+      <a href="../details/detail.html">
+        <img
+          src="${post.Scifiimg}"
+          class="card-img-top product-img"
+          alt="..."
+        />
+      </a>
+      <div class="card-body">
+        <h4 class="card-title name">${post.Scifititle}</h4>
+        <div class="card-text">
+          <p class="desc"><i> ${post.Scifidescription}</i></p>
+
+          <p></p>
+          <p id="price">Price: $${post.Scifiprice}</p>
+          <p id="instock">In Stock: ${post.Scifiinstock}</p>
+        </div>
+      <button type="submit" id="add" onclick="addCart('${postId}')">Add to cart</button>
+      </div>
+    </div>
+  </div>
+    `;
+  });
+
+  const showCart = document.getElementById("showCart");
+  window.addCart1 = async function (id) {
+    const docRef = doc(db, "Books", id);
+    const docSnap = await getDoc(docRef);
+    cartTitle = docSnap.data().Booktitle;
+    cartPrice = docSnap.data().Bookprice;
+    try {
+      const docRef = await addDoc(collection(db, "Cart"), {
+        Carttitle: cartTitle,
+        Cartprice: cartPrice,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+});
+
+document.getElementById("search").addEventListener("click", (e) => {
+  e.preventDefault();
+  let valueSearch = document.getElementById("search-input").value;
+  let cards = document.querySelectorAll(".card");
+  let names = document.querySelectorAll(".name");
+
+  for (let i = 0; i < cards.length; i++) {
+    let aProductName = names[i].innerHTML;
+
+    if (aProductName.includes(valueSearch)) {
+      cards[i].classList.remove("hide");
+      console.log(cards[i]);
+    } else {
+      cards[i].classList.add("hide");
+    }
+  }
+});
